0274eba0834960a6a6cff8324a9e3a3702562f46
mr-gantiva <ivaganva@gmail.com>
2022-10-23 21:30:56 -0300

    Remplazando funciones en JavaScript para encriptar y desencriptar mediante matrices

--- js/script.js
+++ js/script.js
@@ -4,63 +4,54 @@
 // i - imes
 // o - ober
 // u - ufat
-// Orden para evitar duplicidad en la encriptación
-// e --> enter
-// o --> ober
-// i --> imes
-// a --> ai
-// u --> ufat
+
+const inputText = document.querySelector(".input-text");
+const message = document.querySelector(".message");
 
 // Función para encriptar
-function encryptText(){
-    var texto = document.getElementById("inputText").value.toLowerCase();
-    //i es ppara afectar tanto a mayusculas como minusculas
-    //g para toda la oración
-    //m para que afecte multiples lineas
-    var txtEncrypt = texto.replace(/e/igm,"enter");
-    var txtEncrypt = txtEncrypt.replace(/o/igm,"ober");
-    var txtEncrypt = txtEncrypt.replace(/i/igm,"imes");
-    var txtEncrypt = txtEncrypt.replace(/a/igm,"ai");
-    var txtEncrypt = txtEncrypt.replace(/u/igm,"ufat");
+function btnEncrypt(){
+    const encryptText = encrypt(inputText.value);
+    message.value = encryptText;
+    message.style.backgroundImage = "none";
+    inputText.value = "";
+}
+function encrypt(stringEncrypt){
+    /*let matrizCode = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];*/
+    stringEncrypt = stringEncrypt.toLowerCase();
 
-    //Retirar imagen cada vez que ingrese texto en contenedor derecho
-    document.getElementById("imgDoll").style.display = "none";
-    //Retirar texto "Ningún mensaje fue encontrado"
-    document.getElementById("textoMensaje").style.display = "none";
-    //Muestra el resultado del texto cifrado
-    document.getElementById("textoMensaje2").innerHTML = txtEncrypt;
-    //Mostrar el boton para copiar el texto contenedor derecho
-    document.getElementById("btnCopiar").style.display = "show";
-    document.getElementById("btnCopiar").style.display = "inherit";
+    for(let i = 0; i < matrizCode.length; i++){
+        if(stringEncrypt.includes(matrizCode[i][0])){
+            stringEncrypt = stringEncrypt.replaceAll(matrizCode[i][0],matrizCode[i][1]);
+        }
+    }
+    return stringEncrypt;
 }
 
 //Función para desencriptar
-function decryptText(){
-    var texto = document.getElementById("inputText").value.toLowerCase();
-    //i es ppara afectar tanto a mayusculas como minusculas
-    //g para toda la oración
-    //m para que afecte multiples lineas
-    var txtEncrypt = texto.replace(/enter/igm,"e");
-    var txtEncrypt = txtEncrypt.replace(/ober/igm,"o");
-    var txtEncrypt = txtEncrypt.replace(/imes/igm,"i");
-    var txtEncrypt = txtEncrypt.replace(/ai/igm,"a");
-    var txtEncrypt = txtEncrypt.replace(/ufat/igm,"u");
 
-    //Retirar imagen cada vez que ingrese texto en contenedor derecho
-    document.getElementById("imgDoll").style.display = "none";
-    //Retirar texto "Ningún mensaje fue encontrado"
-    document.getElementById("textoMensaje").style.display = "none";
-    //Muestra el resultado del texto cifrado
-    document.getElementById("textoMensaje2").innerHTML = txtEncrypt;
-    //Mostrar el boton para copiar el texto contenedor derecho
-    document.getElementById("btnCopiar").style.display = "show";
-    document.getElementById("btnCopiar").style.display = "inherit";
+function btnDecrypt(){
+    const decryptText = decrypt(inputText.value);
+    message.value = decryptText;
+    inputText.value = "";
+}
+
+
+function decrypt(stringDecrypt){
+    let matrizCode = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDecrypt = stringDecrypt.toLowerCase();
+
+    for(let i = 0; i < matrizCode.length; i++){
+        if(stringDecrypt.includes(matrizCode[i][1])){
+            stringDecrypt = stringDecrypt.replaceAll(matrizCode[i][1],matrizCode[i][0]);
+        }
+    }
+    return stringDecrypt;
 }
 
 function copyText(){
-    var content = document.querySelector("#textoMensaje2");
-    content.select();
-    document.execCommand("copy");
-    alert("Se copio");
+    message.select();
+    navigator.clipboard.writeText(message.value);
+    message.value = "";
+    alert("Texto copiado");
 }
 
